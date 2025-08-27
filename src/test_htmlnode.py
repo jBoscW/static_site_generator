f7ebe305a860fca157b471ad1ed747c5f165db9f
jBoscW <jbd393@nyu.edu>
2025-08-27 12:07:49 -0300

    Add htmlnode and leaf node classes

---
+++ src/test_htmlnode.py
@@ -0,0 +1,26 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_NOT_eq(self):
+        node1 = HTMLNode()
+        node2 = HTMLNode()
+        self.assertNotEqual(node1, node2)
+
+    def test_props_to_html_none(self):
+        node = HTMLNode(tag="p", value="Hello")
+        self.assertNotEqual(node.props_to_html(), "ooga booga")
+    
+    def test_props_to_html(self):
+        node = HTMLNode(tag="a", value="Link", props={"href": "https://example.com", "target": "_blank"})
+        self.assertEqual(node.props_to_html(), ' href="https://example.com" target="_blank"')
+    
+    def test_repr(self):
+        node = HTMLNode(tag="p", value="Hello", children=None, props={"class": "text"})
+        self.assertEqual(repr(node), "HTMLNode(tag = p, value = Hello, children = None, props = {'class': 'text'})")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/test_leafnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestTextNode(unittest.TestCase):
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("li", "Hello, world!")
+        self.assertEqual(node.to_html(), "<li>Hello, world!</li>")
+    
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", props={"href": "https://example.com"})
+        self.assertEqual(node.to_html(), '<a href="https://example.com">Click me!</a>')
+    
+    def test_leaf_to_html_and_super(self): 
+        node1 = LeafNode("p", "Hello, world!", props={"class": "text"})
+        node2 = LeafNode("p", "Hello, world!", props={"class": "text"})
+        
+        self.assertNotEqual(node1, node2)
+        self.assertEqual(node1.props_to_html(), ' class="text"')
+        self.assertEqual(node1.value, "Hello, world!")
+        self.assertEqual(node2.children, None)
+    
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Just some text")
+        self.assertEqual(node.to_html(), "Just some text")
+        
