58e2331eea06ce588d8192ca5a9577d8055daec2
jBoscW <jbd393@nyu.edu>
2025-09-10 14:43:45 -0300

    Add github pages functionality

--- src/main.py
+++ src/main.py
@@ -5,16 +5,14 @@ def main(basepath = '/'):
     if len(sys.argv) > 1: 
         basepath = sys.argv[1]
 
-
-
     delete_and_move(public_dir='./public', static_dir='./static')
 
     generate_pages_recursive(
-        dir_path_content='./content', 
-        dest_dir_path='./public', 
+        dir_path_content = './content', 
+        dest_dir_path = './public', 
+        base_path = basepath,
         template_path = './template.html'
     )
-
 
 
 if __name__ == "__main__":
